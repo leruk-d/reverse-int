af6a47fce77415a9f0fdf7dec53bd6bdc07d3335
leruk-d <lerka3003@mail.ru>
2023-01-15 16:07:38 +0300

    decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let string = String(n);
+    let arr = [];
+    arr = Array.from(string);
+    console.log(arr);
+    return +arr.map
+        .call(string, (x) => x)
+        .reverse()
+        .join("");
+};
